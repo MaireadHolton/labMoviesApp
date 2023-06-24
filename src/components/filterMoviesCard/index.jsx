f9b02b68377bffb5642218bb8adc059147c40dce
Mairead Holton <97246575+MaireadHolton@users.noreply.github.com>
2023-06-24 20:28:59 +0100

    Using react-query to manage browser-based cache.

--- src/components/filterMoviesCard/index.jsx
+++ src/components/filterMoviesCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }from "react";
+import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -7,8 +7,9 @@ import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import SortIcon from '@mui/icons-material/Sort';
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 import { getGenres } from "../../api/tmdb-api";
-
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
@@ -26,27 +27,33 @@ const styles = {
 };
 
 export default function FilterMoviesCard(props) {
-  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
+  const { data, error, isLoading, isError } = useQuery("genres", getGenres);
 
-  useEffect(() => {
-    getGenres().then((allGenres) => {
-      setGenres([genres[0], ...allGenres]);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const handleChange = (e, type, value) => {
-    e.preventDefault()
-    props.onUserInput(type, value)
-  };
-
-  const handleTextChange = e => {
-    handleChange(e, "title", e.target.value)
+  if (isLoading) {
+    return <Spinner />;
   }
 
-  const handleGenreChange = e => {
-    handleChange(e, "genre", e.target.value)
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const genres = data.genres;
+  if (genres[0].name !== "All") {
+    genres.unshift({ id: "0", name: "All" });
+  }
+
+  const handleUserImput = (e, type, value) => {
+    e.preventDefault();
+    props.onUserInput(type, value); // NEW
   };
+
+  const handleTextChange = (e, props) => {
+    handleUserImput(e, "title", e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    handleUserImput(e, "genre", e.target.value);
+  };
+
 
   return (
     <>
